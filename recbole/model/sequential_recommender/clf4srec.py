0367a2357d74e81bb4975bc3250e64fbb3b796f8
zhangyichi1Z <815153707@qq.com>
2023-04-03 10:15:42 +0800

    CLF4SRec

---
+++ recbole/model/sequential_recommender/clf4srec.py
@@ -0,0 +1,331 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/18 11:33
+# @Author  : Hui Wang
+# @Email   : hui.wang@ruc.edu.cn
+
+"""
+SASRec
+################################################
+
+Reference:
+    Wang-Cheng Kang et al. "Self-Attentive Sequential Recommendation." in ICDM 2018.
+
+Reference:
+    https://github.com/kang205/SASRec
+
+"""
+
+import math
+import random
+
+import numpy as np
+import torch
+from torch import nn
+
+from recbole.model.abstract_recommender import SequentialRecommender
+from recbole.model.layers import TransformerEncoder
+from recbole.model.loss import BPRLoss
+import torch.fft as fft
+import torch.nn.functional as F
+
+
+class CLF4SRec(SequentialRecommender):
+    r"""
+    SASRec is the first sequential recommender based on self-attentive mechanism.
+
+    NOTE:
+        In the author's implementation, the Point-Wise Feed-Forward Network (PFFN) is implemented
+        by CNN with 1x1 kernel. In this implementation, we follows the original BERT implementation
+        using Fully Connected Layer to implement the PFFN.
+    """
+
+    def __init__(self, config, dataset):
+        super(CLF4SRec, self).__init__(config, dataset)
+
+        # load parameters info
+        self.n_layers = config['n_layers']
+        self.n_heads = config['n_heads']
+        self.hidden_size = config['hidden_size']  # same as embedding_size
+        self.inner_size = config['inner_size']  # the dimensionality in feed-forward layer
+        self.hidden_dropout_prob = config['hidden_dropout_prob']
+        self.attn_dropout_prob = config['attn_dropout_prob']
+        self.hidden_act = config['hidden_act']
+        self.layer_norm_eps = config['layer_norm_eps']
+
+        self.batch_size = config['train_batch_size']
+        self.lmd = config['lmd']
+        self.lmd_tf = config['lmd_tf']
+        self.tau = config['tau']
+        self.sim = config['sim']
+
+        self.tau_plus = config['tau_plus']
+        self.beta = config['beta']
+
+        self.initializer_range = config['initializer_range']
+        self.loss_type = config['loss_type']
+
+        # define layers and loss
+        self.item_embedding = nn.Embedding(self.n_items + 1, self.hidden_size, padding_idx=0)
+        self.position_embedding = nn.Embedding(self.max_seq_length, self.hidden_size)
+        self.trm_encoder = TransformerEncoder(
+            n_layers=self.n_layers,
+            n_heads=self.n_heads,
+            hidden_size=self.hidden_size,
+            inner_size=self.inner_size,
+            hidden_dropout_prob=self.hidden_dropout_prob,
+            attn_dropout_prob=self.attn_dropout_prob,
+            hidden_act=self.hidden_act,
+            layer_norm_eps=self.layer_norm_eps
+        )
+
+        self.fft_layer = BandedFourierLayer(self.hidden_size, self.hidden_size, 0, 1, length=self.max_seq_length)
+        self.LayerNorm = nn.LayerNorm(self.hidden_size, eps=self.layer_norm_eps)
+        self.dropout = nn.Dropout(self.hidden_dropout_prob)
+
+        if self.loss_type == 'BPR':
+            self.loss_fct = BPRLoss()
+        elif self.loss_type == 'CE':
+            self.loss_fct = nn.CrossEntropyLoss()
+        else:
+            raise NotImplementedError("Make sure 'loss_type' in ['BPR', 'CE']!")
+
+        self.mask_default = self.mask_correlated_samples(batch_size=self.batch_size)
+        self.nce_fct = nn.CrossEntropyLoss()
+
+        # parameters initialization
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        """ Initialize the weights """
+        if isinstance(module, (nn.Linear, nn.Embedding)):
+            # Slightly different from the TF version which uses truncated_normal for initialization
+            # cf https://github.com/pytorch/pytorch/pull/5617
+            module.weight.data.normal_(mean=0.0, std=self.initializer_range)
+        elif isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+        if isinstance(module, nn.Linear) and module.bias is not None:
+            module.bias.data.zero_()
+
+    def get_attention_mask(self, item_seq):
+        """Generate left-to-right uni-directional attention mask for multi-head attention."""
+        attention_mask = (item_seq > 0).long()
+        extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2)  # torch.int64
+        # mask for left-to-right unidirectional
+        max_len = attention_mask.size(-1)
+        attn_shape = (1, max_len, max_len)
+        subsequent_mask = torch.triu(torch.ones(attn_shape), diagonal=1)  # torch.uint8
+        subsequent_mask = (subsequent_mask == 0).unsqueeze(1)
+        subsequent_mask = subsequent_mask.long().to(item_seq.device)
+
+        extended_attention_mask = extended_attention_mask * subsequent_mask
+        extended_attention_mask = extended_attention_mask.to(dtype=next(self.parameters()).dtype)  # fp16 compatibility
+        extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
+        return extended_attention_mask
+
+    def forward(self, item_seq, item_seq_len):
+        position_ids = torch.arange(item_seq.size(1), dtype=torch.long, device=item_seq.device)
+        position_ids = position_ids.unsqueeze(0).expand_as(item_seq)
+        position_embedding = self.position_embedding(position_ids)
+
+        item_emb = self.item_embedding(item_seq)
+        input_emb = item_emb + position_embedding
+        input_emb = self.LayerNorm(input_emb)
+        input_emb = self.dropout(input_emb)
+
+        extended_attention_mask = self.get_attention_mask(item_seq)
+
+        trm_output = self.trm_encoder(input_emb, extended_attention_mask, output_all_encoded_layers=True)
+        output_t = trm_output[-1]
+        output_t = self.gather_indexes(output_t, item_seq_len - 1)
+
+        output_f = self.fft_layer(input_emb)
+        trm_output_f = self.trm_encoder(output_f, extended_attention_mask, output_all_encoded_layers=True)
+        output_f = trm_output_f[-1]
+        output_f = self.gather_indexes(output_f, item_seq_len - 1)
+
+        return output_t, output_f  # [B H]
+
+    def my_fft(self, seq):
+        f = torch.fft.rfft(seq, dim=1)
+        amp = torch.absolute(f)
+        phase = torch.angle(f)
+        return amp, phase
+
+    def calculate_loss(self, interaction):
+        item_seq = interaction[self.ITEM_SEQ]
+        item_seq_len = interaction[self.ITEM_SEQ_LEN]
+        seq_output_t, seq_output_f = self.forward(item_seq, item_seq_len)
+        pos_items = interaction[self.POS_ITEM_ID]
+
+        if self.loss_type == 'BPR':
+            neg_items = interaction[self.NEG_ITEM_ID]
+            pos_items_emb = self.item_embedding(pos_items)
+            neg_items_emb = self.item_embedding(neg_items)
+            pos_score = torch.sum(seq_output_t * pos_items_emb, dim=-1)  # [B]
+            neg_score = torch.sum(seq_output_t * neg_items_emb, dim=-1)  # [B]
+            loss = self.loss_fct(pos_score, neg_score)
+        else:  # self.loss_type = 'CE'
+
+            test_item_emb = self.item_embedding.weight[:self.n_items]  # unpad the augmentation mask
+            logits = torch.matmul(seq_output_t, test_item_emb.transpose(0, 1))
+            loss = self.loss_fct(logits, pos_items)
+
+        # NCE
+
+        # Time ssl
+        aug_item_seq, aug_len = interaction['aug'], interaction['aug_len']
+        aug_seq_output_t, aug_seq_output_f = self.forward(aug_item_seq, aug_len)
+        nce_logits_t, nce_labels_t = self.info_nce(aug_seq_output_t, seq_output_t, temp=self.tau,
+                                                   batch_size=seq_output_t.shape[0],
+                                                   sim=self.sim)
+        nce_loss_t = self.nce_fct(nce_logits_t, nce_labels_t)
+
+        # Time-Frequency ssl
+        nce_logits_t_f, nce_labels_t_f = self.info_nce(seq_output_f, seq_output_t, temp=self.tau,
+                                                       batch_size=seq_output_t.shape[0],
+                                                       sim=self.sim)
+        nce_loss_t_f = self.nce_fct(nce_logits_t_f, nce_labels_t_f)
+
+        # Frequency ssl
+        f_aug_seq_output_amp, f_aug_seq_output_phase = self.my_fft(seq_output_t)
+        f_seq_output_amp, f_seq_output_phase = self.my_fft(seq_output_f)
+
+        # Amp ssl
+        nce_logits_amp, nce_labels_amp = self.info_nce(f_aug_seq_output_amp, f_seq_output_amp, temp=self.tau,
+                                                       batch_size=seq_output_t.shape[0],
+                                                       sim=self.sim)
+        nce_loss_amp = self.nce_fct(nce_logits_amp, nce_labels_amp)
+
+        # Phase ssl
+        nce_logits_phase, nce_labels_phase = self.info_nce(f_aug_seq_output_phase, f_seq_output_phase, temp=self.tau,
+                                                           batch_size=seq_output_t.shape[0],
+                                                           sim=self.sim)
+        nce_loss_phase = self.nce_fct(nce_logits_phase, nce_labels_phase)
+
+        return loss + self.lmd/2 * (
+                    self.lmd_tf * nce_loss_t + (1 - self.lmd_tf)/3 * (nce_loss_t_f + nce_loss_phase + nce_loss_amp))
+
+    def decompose(self, z_i, z_j, origin_z, batch_size):
+        """
+        We do not sample negative examples explicitly.
+        Instead, given a positive pair, similar to (Chen et al., 2017), we treat the other 2(N − 1) augmented examples within a minibatch as negative examples.
+        """
+        N = 2 * batch_size
+
+        z = torch.cat((z_i, z_j), dim=0)
+
+        # pairwise l2 distace
+        sim = torch.cdist(z, z, p=2)
+
+        sim_i_j = torch.diag(sim, batch_size)
+        sim_j_i = torch.diag(sim, -batch_size)
+
+        positive_samples = torch.cat((sim_i_j, sim_j_i), dim=0).reshape(N, 1)
+        alignment = positive_samples.mean()
+
+        # pairwise l2 distace
+        sim = torch.cdist(origin_z, origin_z, p=2)
+        mask = torch.ones((batch_size, batch_size), dtype=bool)
+        mask = mask.fill_diagonal_(0)
+        negative_samples = sim[mask].reshape(batch_size, -1)
+        uniformity = torch.log(torch.exp(-2 * negative_samples).mean())
+
+        return alignment, uniformity
+
+    def mask_correlated_samples(self, batch_size):
+        N = 2 * batch_size
+        mask = torch.ones((N, N), dtype=bool)
+        mask = mask.fill_diagonal_(0)
+        for i in range(batch_size):
+            mask[i, batch_size + i] = 0
+            mask[batch_size + i, i] = 0
+        return mask
+
+    def info_nce(self, z_i, z_j, temp, batch_size, sim='dot'):
+        """
+        We do not sample negative examples explicitly.
+        Instead, given a positive pair, similar to (Chen et al., 2017), we treat the other 2(N − 1) augmented examples within a minibatch as negative examples.
+        """
+        N = 2 * batch_size
+
+        z = torch.cat((z_i, z_j), dim=0)
+
+        if sim == 'cos':
+            sim = nn.functional.cosine_similarity(z.unsqueeze(1), z.unsqueeze(0), dim=2) / temp
+        elif sim == 'dot':
+            sim = torch.mm(z, z.T) / temp
+
+        sim_i_j = torch.diag(sim, batch_size)
+        sim_j_i = torch.diag(sim, -batch_size)
+
+        positive_samples = torch.cat((sim_i_j, sim_j_i), dim=0).reshape(N, 1)
+        if batch_size != self.batch_size:
+            mask = self.mask_correlated_samples(batch_size)
+        else:
+            mask = self.mask_default
+        negative_samples = sim[mask].reshape(N, -1)
+
+        labels = torch.zeros(N).to(positive_samples.device).long()
+        logits = torch.cat((positive_samples, negative_samples), dim=1)
+        return logits, labels
+
+    def predict(self, interaction):
+        item_seq = interaction[self.ITEM_SEQ]
+        item_seq_len = interaction[self.ITEM_SEQ_LEN]
+        test_item = interaction[self.ITEM_ID]
+        seq_output_t, seq_output_f = self.forward(item_seq, item_seq_len)
+        test_item_emb = self.item_embedding(test_item)
+        scores = torch.mul(seq_output_t, test_item_emb).sum(dim=1)  # [B]
+        return scores
+
+    def full_sort_predict(self, interaction):
+        item_seq = interaction[self.ITEM_SEQ]
+        item_seq_len = interaction[self.ITEM_SEQ_LEN]
+        seq_output_t, seq_output_f = self.forward(item_seq, item_seq_len)
+        test_items_emb = self.item_embedding.weight[:self.n_items]  # unpad the augmentation mask
+        scores = torch.matmul(seq_output_t, test_items_emb.transpose(0, 1))  # [B n_items]
+        return scores
+
+
+class BandedFourierLayer(nn.Module):
+    def __init__(self, in_channels, out_channels, band, num_bands, length=201):
+        super().__init__()
+
+        self.length = length
+        self.total_freqs = (self.length // 2) + 1
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+        self.band = band  # zero indexed
+        self.num_bands = num_bands
+
+        self.num_freqs = self.total_freqs // self.num_bands + (
+            self.total_freqs % self.num_bands if self.band == self.num_bands - 1 else 0)
+
+        self.start = self.band * (self.total_freqs // self.num_bands)
+        self.end = self.start + self.num_freqs
+
+        # case: from other frequencies
+        self.weight = nn.Parameter(torch.empty((self.num_freqs, in_channels, out_channels), dtype=torch.cfloat))
+        self.bias = nn.Parameter(torch.empty((self.num_freqs, out_channels), dtype=torch.cfloat))
+        self.reset_parameters()
+
+    def forward(self, input):
+        # input - b t d
+        b, t, _ = input.shape
+        input_fft = fft.rfft(input, dim=1)
+        output_fft = torch.zeros(b, t // 2 + 1, self.out_channels, device=input.device, dtype=torch.cfloat)
+        output_fft[:, self.start:self.end] = self._forward(input_fft)
+        return fft.irfft(output_fft, n=input.size(1), dim=1)
+
+    def _forward(self, input):
+        output = torch.einsum('bti,tio->bto', input[:, self.start:self.end], self.weight)
+        return output + self.bias
+
+    def reset_parameters(self) -> None:
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+        bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+        nn.init.uniform_(self.bias, -bound, bound)
